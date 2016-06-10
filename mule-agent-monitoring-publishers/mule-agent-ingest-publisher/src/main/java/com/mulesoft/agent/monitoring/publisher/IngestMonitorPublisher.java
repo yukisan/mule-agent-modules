a5e66687b277e109c8fbeeb2b963718394dcb81c
Sebastian G. Vinci <sgvinci@gmail.com>
2016-06-10 15:10:39 -0300

    AGENT-313 Exceptions are thrown when Tests run in mule-agent (#42)
    * AGENT-313 disable ingest monitor publishers if configuration is invalid
    
    * AGENT-313 move configuration checking algorithm to single responsibility method
    
    * AGENT-313 move initialization to initialize method instead of postConfigure.

--- mule-agent-monitoring-publishers/mule-agent-ingest-publisher/src/main/java/com/mulesoft/agent/monitoring/publisher/IngestMonitorPublisher.java
+++ mule-agent-monitoring-publishers/mule-agent-ingest-publisher/src/main/java/com/mulesoft/agent/monitoring/publisher/IngestMonitorPublisher.java
@@ -8,10 +8,12 @@ import com.mulesoft.agent.clients.AuthenticationProxyClient;
 import com.mulesoft.agent.configuration.Configurable;
 import com.mulesoft.agent.configuration.PostConfigure;
 import com.mulesoft.agent.configuration.common.SecurityConfiguration;
+import com.mulesoft.agent.handlers.exception.InitializationException;
 import com.mulesoft.agent.handlers.internal.client.DefaultAuthenticationProxyClient;
 import com.mulesoft.agent.monitoring.publisher.ingest.AnypointMonitoringIngestAPIClient;
 import com.mulesoft.agent.monitoring.publisher.ingest.builder.IngestMetricBuilder;
 import com.mulesoft.agent.services.OnOffSwitch;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +30,7 @@ public abstract class IngestMonitorPublisher<T> extends BufferedHandler<T>
     @Configurable("{}")
     private SecurityConfiguration securityConfiguration;
 
-    @Configurable("http://localhost:8088")
+    @Configurable
     private String authProxyEndpoint;
 
     @Configurable("1")
@@ -43,23 +45,36 @@ public abstract class IngestMonitorPublisher<T> extends BufferedHandler<T>
     protected AnypointMonitoringIngestAPIClient client;
 
     @Override
-    @PostConfigure
-    public void postConfigurable() throws AgentEnableOperationException
-    {
-        if(this.enabledSwitch == null)
-        {
-            this.enabledSwitch = OnOffSwitch.newNullSwitch(this.enabled);
-            if (this.buffer == null)
-            {
-                this.buffer = new BufferConfiguration();
-                this.buffer.setType(BufferType.MEMORY);
-                this.buffer.setRetryCount(1);
-                this.buffer.setFlushFrequency(60000L);
-                this.buffer.setMaximumCapacity(100);
-            }
+    public void initialize() throws InitializationException {
+        if (!this.checkConfiguration()) {
+            throw new InitializationException("Could not initialize ingest monitor publisher. Its configuration is invalid.");
         }
         AuthenticationProxyClient authProxyClient = DefaultAuthenticationProxyClient.create(authProxyEndpoint, securityConfiguration);
         this.client = AnypointMonitoringIngestAPIClient.create(apiVersion, authProxyClient);
+        super.initialize();
+    }
+
+    @Override
+    public BufferConfiguration getBuffer() {
+        if (this.buffer == null)
+        {
+            this.buffer = new BufferConfiguration();
+            this.buffer.setType(BufferType.MEMORY);
+            this.buffer.setRetryCount(1);
+            this.buffer.setFlushFrequency(60000L);
+            this.buffer.setMaximumCapacity(100);
+        }
+        return this.buffer;
+    }
+
+    private boolean checkConfiguration() {
+        return this.securityConfiguration != null &&
+                StringUtils.isNotBlank(this.securityConfiguration.getKeyStoreAlias()) &&
+                StringUtils.isNotBlank(this.securityConfiguration.getKeyStoreAliasPassword()) &&
+                StringUtils.isNotBlank(this.securityConfiguration.getKeyStoreFile()) &&
+                StringUtils.isNotBlank(this.securityConfiguration.getKeyStorePassword()) &&
+                StringUtils.isNotBlank(this.securityConfiguration.getTrustStoreFile()) &&
+                StringUtils.isNotBlank(this.authProxyEndpoint);
     }
 
     protected abstract boolean send(Collection<T> collection);
